9ed5247f0fc868e1daea041491282bdb132abfee
Przemek Delewski <pdelewski@sumologic.com>
2023-06-09 17:02:35 +0200

    adding interfaces

--- component/component.go
+++ component/component.go
@@ -2,6 +2,10 @@ package component
 
 import "fmt"
 
+type Factory interface {
+  Type()
+}
+
 func Component() {
   fmt.Println("Component")
 }
--- main.go
+++ main.go
@@ -3,7 +3,20 @@ package main
 import "github.com/pdelewski/instrgen_test_module/module1"
 import "github.com/pdelewski/instrgen_testapp/component"
 
+type Factory interface {
+  component.Factory
+}
+
+type basic struct {
+}
+
+func (b basic) Type() {
+}
+
 func main() {
   module1.Foo()
   component.Component()
+  var b Factory
+  b = basic{}
+  b.Type()
 }
